01de9ad033227a16e51358a768c67d5188a3b2ff
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-02-28 23:00:34 -0300

    criando calculadora com react, ajustando estrutura css

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Button.css";
+
+const Button = (props) => {
+  let classes = 'Button ';
+    classes+=props.operation ? "operation " : "";
+    classes+=props.double ? "double " : "";
+    classes+=props.triple ? "triple " : "";
+
+  return (
+        <button 
+            className={classes}
+            onClick={_=> props.click && props.click(props.label)}
+            >
+            {props.label}
+        </button>
+  );
+};
+
+export default Button;
